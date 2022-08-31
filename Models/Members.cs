8763b7e7891991075db1f179c6fec3d1727ec20c
younz <37065179+younz@users.noreply.github.com>
2022-08-31 10:25:17 +0200

    tilføjet projeket til med indhold

---
+++ Models/Members.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisBooking.Models
+{
+    public class Members
+    {
+        public TYPE Type { get; set; }
+    }
+}
